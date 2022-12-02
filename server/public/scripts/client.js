ef88eea4b02e14de1fab48435369656ddbc43c4e
John Maybee <john.d.maybee@gmail.com>
2022-12-01 22:57:24 -0500

    added a basic html and sourced in the other files. Also added jQuery and fired off the onReady function

---
+++ server/public/scripts/client.js
@@ -0,0 +1,8 @@
+$(document).ready(onReady);
+
+
+////////////////////////////////////////////onReady function///////////////////////////////////////////////
+
+function onReady() {
+    console.log('It is go time!');
+} // onReady function that will trigger on page load due to the $(document).ready(onReady)
