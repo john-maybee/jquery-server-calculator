b3296fdc6073fbabbbd9cc3cf7dea9c0074d1a00
John Maybee <john.d.maybee@gmail.com>
2022-12-04 14:05:45 -0500

    tried to seperate the post/calculations into its own function, then realized it didn't attached the newEquation data properly, so i reverted. added the multiplication function as well

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -7,6 +7,7 @@ function onReady() {
     console.log('It is go time!');
     $('#addButton').on('click', addition);
     $('#subtractButton').on('click', subtraction);
+    $('#multiplyButton').on('click', multiply);
 } // onReady function that will trigger on page load due to the $(document).ready(onReady)
 
 
@@ -16,6 +17,9 @@ function addition() {
     console.log('addition function here. hi.');
     $('#equalsButton').on('click', additionEquals);
 }
+
+
+////////////////////////////////////////////additionEquals function//
 
 function additionEquals() {
     console.log('add it up!');
@@ -54,6 +58,9 @@ function subtraction() {
     $('#equalsButton').on('click', subtractionEquals);
 }
 
+
+////////////////////////////////////////////subtractionEquals function//
+
 function subtractionEquals() {
     console.log('take some away!');
     let answer = Number($('#numOneIn').val()) - Number($('#numTwoIn').val());
@@ -81,6 +88,48 @@ function subtractionEquals() {
         
     });
 }
+
+
+///////////////////////////////////////////////multiply function//////////////////////////////////////////////////
+
+function multiply() {
+    console.log('Multiply by Mr. Sheeran');
+    $('#equalsButton').on('click', multiplyEquals);
+}
+
+
+///////////////////////////////////////////////multiplyEquals function//////////////////////////////////////////////////
+
+function multiplyEquals() {
+    console.log('multiply like a Hydra.');
+    let answer = Number($('#numOneIn').val()) * Number($('#numTwoIn').val());
+    let newEquation = {
+        number1: Number($('#numOneIn').val()),
+        operation: '*',
+        number2: Number($('#numTwoIn').val()),
+        equals: '=',
+        result: answer,
+    }
+    console.log(newEquation); // checking to see if this new equation was created as an object that I can then put into the calculationList.js list
+    $('#answerOut').empty(); // empty the answer field on the page
+    $('#answerOut').append(answer); // update the answer field to the current result/answer
+
+    $.ajax({
+        method: 'POST',
+        url: '/calculations',
+        data: newEquation,
+    }).then(function(response) {
+        console.log('this is the post response from the server', response);
+        getCalculations();
+    }).catch(function(error) {
+        console.log(error);
+        alert(error.responseText);
+        
+    });
+}
+
+
+////////////////////////////////////////////getCalculations function///////////////////////////////////////////////
 
 function getCalculations() {
     console.log('in getCalculations');
