387d654d57b1ad79a41481a1580a4c61fe011420
John Maybee <john.d.maybee@gmail.com>
2022-12-04 13:37:32 -0500

    Added in the subtraction function with the - button and posted the calculation to the array. currently the = is pulling more than one equation, but I am going to solve that when I make my clear function

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,15 +1,16 @@
 $(document).ready(onReady);
 
 
-////////////////////////////////////////////onReady function///////////////////////////////////////////////
+///////////////////////////////////////////////onReady function//////////////////////////////////////////////////
 
 function onReady() {
     console.log('It is go time!');
     $('#addButton').on('click', addition);
+    $('#subtractButton').on('click', subtraction);
 } // onReady function that will trigger on page load due to the $(document).ready(onReady)
 
 
-////////////////////////////////////////////addition function///////////////////////////////////////////////
+///////////////////////////////////////////////addition function//////////////////////////////////////////////////
 
 function addition() {
     console.log('addition function here. hi.');
@@ -45,8 +46,44 @@ function additionEquals() {
     });
 } // could use this for each one
 
+
+///////////////////////////////////////////////subtraction function//////////////////////////////////////////////////
+
+function subtraction() {
+    console.log('subtraction can be good.');
+    $('#equalsButton').on('click', subtractionEquals);
+}
+
+function subtractionEquals() {
+    console.log('take some away!');
+    let answer = Number($('#numOneIn').val()) - Number($('#numTwoIn').val());
+    let newEquation = {
+        number1: Number($('#numOneIn').val()),
+        operation: '-',
+        number2: Number($('#numTwoIn').val()),
+        equals: '=',
+        result: answer,
+    }
+    console.log(newEquation); // checking to see if this new equation was created as an object that I can then put into the calculationList.js list
+    $('#answerOut').empty(); // empty the answer field on the page
+    $('#answerOut').append(answer); // update the answer field to the current result/answer
+
+    $.ajax({
+        method: 'POST',
+        url: '/calculations',
+        data: newEquation,
+    }).then(function(response) {
+        console.log('this is the post response from the server', response);
+        getCalculations();
+    }).catch(function(error) {
+        console.log(error);
+        alert(error.responseText);
+        
+    });
+}
+
 function getCalculations() {
-    console.log('in getAddition');
+    console.log('in getCalculations');
     $.ajax({
         method: 'GET',
         url: '/calculations',
