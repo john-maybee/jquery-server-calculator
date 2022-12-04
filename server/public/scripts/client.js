2dc113c8c9a9c01fbbe46b2e43349c060f4b47fe
John Maybee <john.d.maybee@gmail.com>
2022-12-03 22:32:58 -0500

    starting to make the addition button functional. started a function called addition, and a function called additionAnswer that fire off when addition is hit then equals is hit

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -5,4 +5,17 @@ $(document).ready(onReady);
 
 function onReady() {
     console.log('It is go time!');
+    $('#addButton').on('click', addition);
 } // onReady function that will trigger on page load due to the $(document).ready(onReady)
+
+
+////////////////////////////////////////////addition function///////////////////////////////////////////////
+
+function addition() {
+    console.log('addition function here. hi.');
+    $('#equalsButton').on('click', additionAnswer);
+}
+
+function additionAnswer() {
+    console.log('add it up!');
+}
